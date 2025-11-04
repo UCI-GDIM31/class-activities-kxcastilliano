316a72aac81786fd26257fb57ed7afca4c944c2f
kxcastilliano <kxcastil@uci.edu>
2025-11-03 19:19:02 -0800

    Fixed W6

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
-    [SerializeField] private float _batspeed;
-    private transform _player;
-    private bool _isChasing = false;
+    [SerializeField] private float _batspeed = 3.5;
+    private Transform _player;
+
+    public void EnableChase(Transform player)
+    { enabled = true;
+    _playerTransform = player;}
+
+    public void DisableChase()
+    { EnableChase = false;}
 
     private void Update()
+    { transform.position = Vector3.MoveTowards(transform.position, _playerTransform.position, _batspeed * Time.deltaTime); }}
     
 
 
